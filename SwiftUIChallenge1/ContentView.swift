67e634f9485add125ed14d7ac47c57012d1bb1f9
macbookpro <dominik.wojtys@gmail.com>
2021-05-30 20:03:23 +0200

    Initial Commit

---
+++ SwiftUIChallenge1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIChallenge1
+//
+//  Created by MacBook Pro on 30/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
