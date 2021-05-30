71e1529289ab11565b84867a67debe24b6c1f63e
macbookpro <dominik.wojtys@gmail.com>
2021-05-30 23:55:00 +0200

    Add pickers

--- SwiftUIChallenge1/ContentView.swift
+++ SwiftUIChallenge1/ContentView.swift
@@ -17,15 +17,43 @@ struct ContentView: View {
     
     let temperatureScales = TemperatureScale.allCases
     @State var valueToBeConverted = ""
+    @State var temperatueScaleToConvert = 1
+    @State var temperatureScaleConverted = 1
+
+    var convertedValue : Double {
+        return 1
+    }
     
     var body: some View {
         NavigationView {
             Form {
-                
+                Section {
+//
+                    Picker("Choose value", selection: $temperatueScaleToConvert) {
+                        ForEach(0 ..< temperatureScales.count) {
+                            Text(temperatureScales[$0].rawValue)
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
                 
                 Section(header: Text("Value to be converted")) {
                     TextField("Value to be converted", text: $valueToBeConverted)
                 }
+                
+                Section {
+//
+                    Picker("Choose value", selection: $temperatureScaleConverted) {
+                        ForEach(0 ..< temperatureScales.count) {
+                            Text(temperatureScales[$0].rawValue)
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Converted value")) {
+                    Text("\(convertedValue, specifier: "%.2f")")
+                }
+                
+
                 
             }.navigationBarTitle("Converter")
         }
