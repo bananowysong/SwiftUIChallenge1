9c40f38b44e00da741163a022eb8ee650e6c1586
macbookpro <dominik.wojtys@gmail.com>
2021-05-30 23:03:09 +0200

    Add textField

--- SwiftUIChallenge1/ContentView.swift
+++ SwiftUIChallenge1/ContentView.swift
@@ -7,10 +7,28 @@
 
 import SwiftUI
 
+enum TemperatureScale: String, CaseIterable {
+    case celcius = "Celcius"
+    case farenheit = "Farenheit"
+    case kelvin = "Kelvin"
+}
+
 struct ContentView: View {
+    
+    let temperatureScales = TemperatureScale.allCases
+    @State var valueToBeConverted = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                
+                
+                Section(header: Text("Value to be converted")) {
+                    TextField("Value to be converted", text: $valueToBeConverted)
+                }
+                
+            }.navigationBarTitle("Converter")
+        }
     }
 }
 
