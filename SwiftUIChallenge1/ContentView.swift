5c91f6de59defa7298abda22d9757d230a86a108
macbookpro <dominik.wojtys@gmail.com>
2021-06-01 13:37:54 +0200

    Implement conversion

--- SwiftUIChallenge1/ContentView.swift
+++ SwiftUIChallenge1/ContentView.swift
@@ -17,12 +17,25 @@ enum TemperatureScale: String, CaseIterable, Identifiable {
 
 struct ContentView: View {
     
-    @State var valueToBeConverted = ""
+    @State var temperatureToBeConverted = ""
     @State var temperatueScaleToConvert = TemperatureScale.farenheit
     @State var temperatureScaleConverted = TemperatureScale.farenheit
 
-    var convertedValue : Double {
-        return 1
+    var convertedTemperature : Double {
+        
+        let toBeConverted = Double(temperatureToBeConverted) ?? 0
+        
+        
+        switch (temperatueScaleToConvert, temperatureScaleConverted) {
+        case(TemperatureScale.celcius,TemperatureScale.kelvin): return toBeConverted + 273.15
+        case(TemperatureScale.celcius,TemperatureScale.farenheit): return (toBeConverted * 9/5) + 32
+        case(TemperatureScale.kelvin,TemperatureScale.celcius): return toBeConverted - 273.15
+        case(TemperatureScale.kelvin,TemperatureScale.farenheit): return (toBeConverted - 273.15) * 9/5 + 32
+        case(TemperatureScale.farenheit,TemperatureScale.celcius): return (toBeConverted - 32) * 5/9
+        case(TemperatureScale.farenheit,TemperatureScale.kelvin): return (toBeConverted - 32) * 5/9 + 273.15
+        default: return toBeConverted
+        }
+       
     }
     
     var body: some View {
@@ -38,7 +51,8 @@ struct ContentView: View {
                 }
                 
                 Section(header: Text("Temperature to be converted")) {
-                    TextField("Temperature to be converted", text: $valueToBeConverted)
+                    TextField("Temperature to be converted", text: $temperatureToBeConverted)
+                        .keyboardType(.decimalPad)
                 }
                 
                 Section {
@@ -51,7 +65,7 @@ struct ContentView: View {
                 }
                 
                 Section(header: Text("Converted temperature")) {
-                    Text("\(convertedValue, specifier: "%.2f")")
+                    Text("\(convertedTemperature, specifier: "%.2f")")
                 }
                 
 
